994fa3d1a8a726ff9b212693ebd230f5af29eedb
diego.goez <jdiego7118@gmail.com>
2018-03-24 11:26:58 -0500

    Prueba 3

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -4,11 +4,6 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 
-import javax.measure.unit.SI.KILOGRAM
-import javax.measure.quantity.Mass
-import org.jscience.physics.model.RelativisticModel
-import org.jscience.physics.amount.Amount
-
 /**
  * Autores:
  * Sebastian Montoya J.
@@ -18,14 +13,7 @@ import org.jscience.physics.amount.Amount
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc)
 {
   def index() = Action { implicit request: Request[AnyContent] =>
-    RelativisticModel.select()
-    val m = Amount.valueOf("12 GeV").to(KILOGRAM)
-    val testRelativity = s"E=mc^2: 12 GeV = $m"
     Ok(views.html.index())
   }
-
-  def mostrarClima()
-  {
-    println("Hola mundo")
-  }
+  
 }
